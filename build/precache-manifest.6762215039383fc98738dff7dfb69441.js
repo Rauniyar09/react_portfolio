08483369cd066d1f7ba82609e175854248da325d
Rahul Rauniyar <69071433+Rauniyar09@users.noreply.github.com>
2023-02-21 16:46:01 +0530

    Add files via upload

---
+++ build/precache-manifest.6762215039383fc98738dff7dfb69441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a7e388261beac6d15ba",
+    "url": "/static/css/main.dd4e8d32.chunk.css"
+  },
+  {
+    "revision": "2a7e388261beac6d15ba",
+    "url": "/static/js/main.2a7e3882.chunk.js"
+  },
+  {
+    "revision": "a15f0b1bb291ad92819e",
+    "url": "/static/css/1.45d771af.chunk.css"
+  },
+  {
+    "revision": "a15f0b1bb291ad92819e",
+    "url": "/static/js/1.a15f0b1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ab5ea4e6f73d613eba25ee98fb887f4",
+    "url": "/index.html"
+  }
+];
---
+++ src/content_option.js
@@ -0,0 +1,145 @@
+const logotext = "KUNAL";
+const meta = {
+    title: "John Doe",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Kunal Rauniyar",
+    animated: {
+        first: "I love coding",
+        second: "I love cool places to visitt",
+        third: "I develop HTML,css Apps",
+    },
+    description: "Belong from small place but passionate to solve the world problems by coding ",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Started loving code from my first interaction with the code and its's outcome.Ready for world to solve problem.",
+};
+const worktimeline = [
+    {
+        jobtitle: " Web debeloper",
+        date: "2022",
+    },
+  
+    
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 10,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Abit fascinated with UI&UX part to play with it",
+    },
+    {
+        title: "PPT Maker",
+        description: "Well i have visited some seminar and have knowledge on how to present and make PPT.",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kunalrauniyar05@gmail.com",
+    YOUR_FONE: "+977-9812030995",
+    //description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
